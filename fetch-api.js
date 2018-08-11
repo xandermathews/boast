a1e2269a5444890dd31104685924389d02f8199e
Xander <xander@ashnazg.com>
2018-08-11 00:23:11 -0700

    auto-backup orcrist 2018-08-11

--- fetch-api.js
+++ fetch-api.js
@@ -28,15 +28,15 @@ var api = {
 				control.formatter = resp => {
 					return resp.text().then(raw => {
 						try {
-						    return JSON.parse(raw);
+							return JSON.parse(raw);
 						} catch (e) {
-						    return raw;
+							return raw;
 						}
 					});
 				}
 			break;
-            case 'text':
-                control.formatter = resp => resp.text();
+			case 'text':
+				control.formatter = resp => resp.text();
 			break;
 		}
 
@@ -50,23 +50,22 @@ var api = {
 		api.appendHeader(conf, 'Authorization', api.authorization);
 
 		return fetch(url, conf).then(response => {
-		    if (control.orig_body) conf.body = control.orig_body;
-		    return control.formatter(response).then(body => {
-		        var headers = {};
-		        for (var h of response.headers) {
-		            headers[h[0]] = h[1];
-                }
-		        return { body, headers, code: response.status, response, request: {url, conf} };
-            });
-        }, error => {
-            return { code: 0, body: {errors:[error]}, request: {url, conf}, headers: {} };
-        }).then(normalized => {
-            console.log({normalized});
-            if (normalized.body && normalized.body.errors && normalized.body.errors.length) {
-                throw normalized;
-            }
-            return normalized;
-        });
+			if (control.orig_body) conf.body = control.orig_body;
+			return control.formatter(response).then(body => {
+				var headers = {};
+				for (var h of response.headers) {
+					headers[h[0]] = h[1];
+				}
+				return { body, headers, code: response.status, response, request: {url, conf} };
+			});
+		}, error => {
+			return { code: 0, body: {errors:[error]}, request: {url, conf}, headers: {} };
+		}).then(normalized => {
+			if (normalized.body && normalized.body.errors && normalized.body.errors.length) {
+				throw normalized;
+			}
+			return normalized;
+		});
 	},
 
 	post: function(path, body, conf, control) {
@@ -78,14 +77,14 @@ var api = {
 		conf.method = 'POST';
 		if (body !== undefined) conf.body = body;
 		if (typeof body === 'object') {
-		    conf.body = JSON.stringify(body);
-		    control = control || {};
-		    control.orig_body = body;
-        }
+			conf.body = JSON.stringify(body);
+			control = control || {};
+			control.orig_body = body;
+		}
 		return api.get(path, conf, control);
 	},
 
-    postForm: function(path, body, conf, control) {
+	postForm: function(path, body, conf, control) {
 		if (arguments.length === 3 && typeof conf === 'string') {
 			control = conf;
 			conf = {};
--- jquery/gen.js
+++ jquery/gen.js
@@ -1,62 +1,62 @@
 "use strict";
 
 function gen(key, attrs, parent) {
-    key = key || 'div';
-    attrs = attrs || {};
+	key = key || 'div';
+	attrs = attrs || {};
 
-    key = key.split('/');
-    if (key.length > 1) {
-        var node = gen(key.shift(), null, parent);
-        while (key.length > 1) {
-            node = node.gen(key.shift());
-        }
-        return node.gen(key.shift(), attrs);
-    }
+	key = key.split('/');
+	if (key.length > 1) {
+		var node = gen(key.shift(), null, parent);
+		while (key.length > 1) {
+			node = node.gen(key.shift());
+		}
+		return node.gen(key.shift(), attrs);
+	}
 
-    key = key[0].split('.');
-    var tag = key[0] || 'div';
-    key.shift();
-    if (key.length) attrs.class = key.join(' ');
-    var ele = $('<'+tag+'>', attrs);
-    if (parent) ele.appendTo($(parent));
-    ele.gen = function(key, attrs) {
-        var child = gen(key, attrs, ele);
-        if (child[0].tagName === 'TABLE') {
-            child.horizontalRecords = function(schema, list) {
-                var data_keys = [];
-                var header = child.gen('tr');
+	key = key[0].split('.');
+	var tag = key[0] || 'div';
+	key.shift();
+	if (key.length) attrs.class = key.join(' ');
+	var ele = $('<'+tag+'>', attrs);
+	if (parent) ele.appendTo($(parent));
+	ele.gen = function(key, attrs) {
+		var child = gen(key, attrs, ele);
+		if (child[0].tagName === 'TABLE') {
+			child.horizontalRecords = function(schema, list) {
+				var data_keys = [];
+				var header = child.gen('tr');
 
-                var col_names = Object.keys(schema);
-                col_names.forEach(k => {
-                    data_keys.push(schema[k]);
-                    header.gen('th', {text: k});
-                });
+				var col_names = Object.keys(schema);
+				col_names.forEach(k => {
+					data_keys.push(schema[k]);
+					header.gen('th', {text: k});
+				});
 
-                return list.map(row => {
-                    var tr = child.gen('tr');
-                    col_names.map(name => {
-                        var data_key = schema[name];
-                        if (typeof data_key === 'function') {
-                            var result = data_key(row, tr);
-                            if (result) tr.gen('td').append(result);
-                        } else {
-                            tr.gen('td', {text: row[data_key]});
-                        }
-                    });
+				return list.map(row => {
+					var tr = child.gen('tr');
+					col_names.map(name => {
+						var data_key = schema[name];
+						if (typeof data_key === 'function') {
+							var result = data_key(row, tr);
+							if (result) tr.gen('td').append(result);
+						} else {
+							tr.gen('td', {text: row[data_key]});
+						}
+					});
 					return tr;
-                });
-            };
+				});
+			};
 
-            child.verticalRecord = function(tup) {
-                Object.keys(tup).filter(k => tup[k] != undefined).map(k => {
-                    var tr = child.gen('tr');
-                    tr.gen('td', {text: k});
-                    tr.gen('td', {text: tup[k]});
-                });
-                return child;
-            };
-        }
-        return child;
-    };
-    return ele;
+			child.verticalRecord = function(tup) {
+				Object.keys(tup).filter(k => tup[k] != undefined).map(k => {
+					var tr = child.gen('tr');
+					tr.gen('td', {text: k});
+					tr.gen('td', {text: tup[k]});
+				});
+				return child;
+			};
+		}
+		return child;
+	};
+	return ele;
 }
